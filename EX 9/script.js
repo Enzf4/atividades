a386c6036a04accd9b83391fcfa2866860209b1a
Enzo Farias <enzo.farias.r@gmail.com>
2023-11-06 15:43:05 -0300

    Exercício operações

---
+++ EX 9/script.js
@@ -0,0 +1,9 @@
+let op1 = 4 + 5;
+let op2 = 4 - 5;
+let op3 = 4 * 5;
+let op4 = 4 / 5;
+
+console.log(`4 + 5 = ${op1}`);
+console.log(`4 - 5 = ${op2}`);
+console.log(`4 x 5 = ${op3}`);
+console.log(`4 / 5 = ${op4}`);
