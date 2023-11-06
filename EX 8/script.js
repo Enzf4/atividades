69e165b73be4ebce1c3fc446552368079dbb2dcb
Enzo Farias <enzo.farias.r@gmail.com>
2023-11-06 15:25:59 -0300

    Exercício 8

---
+++ EX 8/script.js
@@ -0,0 +1,9 @@
+const h1 = document.getElementById('titulo');
+const a = document.querySelector('a');
+const ul = document.querySelector('ul');
+const ol = document.querySelector('ol');
+
+h1.innerText = "Proz Educação";
+a.innerHTML = "Acessar";
+ul.innerHTML = "<li>Item 1</li> <li>Item 2</li> <li>Item 3</li>"
+ol.innerHTML = "<a href='#'><li>Item 1</li></a> <a href='#'><li>Item 2</li></a> <a href='#'><li>Item 3</li></a>"
